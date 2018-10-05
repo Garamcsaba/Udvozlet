f8ae1355c43e3abacec2bba69ee8ce1b3cf909b0
Garamcsaba <43179258+Garamcsaba@users.noreply.github.com>
2018-10-05 18:10:01 +0200

    új nevek

--- src/udvozlet/Udvozlet.java
+++ src/udvozlet/Udvozlet.java
@@ -9,8 +9,8 @@ public class Udvozlet {
 
 
     public static void main(String[] args) {
-        ember no= new ember("Éva");
-        ember ferfi = new ember("Ádám");
+        ember no= new ember("Emma");
+        ember ferfi = new ember("Péter");
         
         System.out.println(no.koszont());
         System.out.println(ferfi.koszont());

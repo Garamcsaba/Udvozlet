366e35f06d02715aa770788febc0669cee3cb857
Garamcsaba <garam.csaba@gmail.hu>
2018-10-05 17:50:59 +0200

    Első változat.Egy osztály.

---
+++ src/udvozlet/Udvozlet.java
@@ -0,0 +1,15 @@
+
+package udvozlet;
+
+/**
+ *
+ * @author t1
+ */
+public class Udvozlet {
+
+
+    public static void main(String[] args) {
+        System.out.println("Üdvözöllek!");
+    }
+    
+}
